edb081611cdb240b9ae0399d2431be235eaaa6ef
Demonlord5556 <acharjeeaditya159@gmail.com>
2026-07-03 23:14:17 +0530

    Adjust sound volume in toggleLight function
    Increased the volume of the sound effect when toggling the light.

--- kot.js
+++ kot.js
@@ -1,7 +1,7 @@
 // Change the text in the quotes to your exact file name
 const soundOn = new Audio('rbd.mpeg'); 
 const soundOff = new Audio('rbd.mpeg');
-lampsound.volume=0.2; // Or use the same file name here
+lampsound.volume=1.0; // Or use the same file name here
 
 function toggleLight() {
     document.body.classList.toggle('light-on');
